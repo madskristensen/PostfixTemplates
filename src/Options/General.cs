33b1026b2af2b81c905e34f52cc91a235a0ed7bb
Mads Kristensen <madsk@microsoft.com>
2026-04-07 15:52:08 -0700

    refactor(options): group template toggles by category
    Refactored General.cs to organize template enablement properties into logical categories using [Category] attributes, such as Control Flow, Loops, Null and Empty Checks, and others. This improves the structure and discoverability of options in the UI. No changes to logic or functionality were made.

--- src/Options/General.cs
+++ src/Options/General.cs
@@ -13,251 +13,267 @@ namespace PostfixTemplates
 
     public class General : BaseOptionModel<General>
     {
-        [Category("Templates")]
+        // Control Flow
+
+        [Category("Control Flow")]
         [DisplayName("Enable '.if'")]
         [Description("Checks boolean expression to be 'true'")]
         [DefaultValue(true)]
         public bool EnableIf { get; set; } = true;
 
-        [Category("Templates")]
+        [Category("Control Flow")]
         [DisplayName("Enable '.else'")]
         [Description("Checks boolean expression to be 'false'")]
         [DefaultValue(true)]
         public bool EnableElse { get; set; } = true;
 
-        [Category("Templates")]
-        [DisplayName("Enable '.var'")]
-        [Description("Introduces variable for expression")]
-        [DefaultValue(true)]
-        public bool EnableVar { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.not'")]
-        [Description("Negates boolean expression")]
-        [DefaultValue(true)]
-        public bool EnableNot { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.null'")]
-        [Description("Checks expression to be null")]
-        [DefaultValue(true)]
-        public bool EnableNull { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.notnull'")]
-        [Description("Checks expression to be not null")]
-        [DefaultValue(true)]
-        public bool EnableNotNull { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.return'")]
-        [Description("Returns expression from current function")]
-        [DefaultValue(true)]
-        public bool EnableReturn { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.foreach'")]
-        [Description("Iterates over enumerable collection")]
-        [DefaultValue(true)]
-        public bool EnableForEach { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.while'")]
-        [Description("Iterates while boolean expression is 'true'")]
-        [DefaultValue(true)]
-        public bool EnableWhile { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.throw'")]
-        [Description("Throws expression of 'Exception' type")]
-        [DefaultValue(true)]
-        public bool EnableThrow { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.arg'")]
-        [Description("Surrounds expression with invocation")]
-        [DefaultValue(true)]
-        public bool EnableArg { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.await'")]
-        [Description("Awaits expressions of 'Task' type")]
-        [DefaultValue(true)]
-        public bool EnableAwait { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.cast'")]
-        [Description("Surrounds expression with cast")]
-        [DefaultValue(true)]
-        public bool EnableCast { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.field'")]
-        [Description("Introduces field for expression")]
-        [DefaultValue(true)]
-        public bool EnableField { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.for'")]
-        [Description("Iterates over collection with index")]
-        [DefaultValue(true)]
-        public bool EnableFor { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.forr'")]
-        [Description("Iterates over collection in reverse with index")]
-        [DefaultValue(true)]
-        public bool EnableForR { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.inject'")]
-        [Description("Introduces primary constructor parameter of type")]
-        [DefaultValue(true)]
-        public bool EnableInject { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.lock'")]
-        [Description("Surrounds expression with lock block")]
-        [DefaultValue(true)]
-        public bool EnableLock { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.new'")]
-        [Description("Produces instantiation expression for type")]
-        [DefaultValue(true)]
-        public bool EnableNew { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.par'")]
-        [Description("Parenthesizes current expression")]
-        [DefaultValue(true)]
-        public bool EnablePar { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.parse'")]
-        [Description("Parses string as value of some type")]
-        [DefaultValue(true)]
-        public bool EnableParse { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.prop'")]
-        [Description("Introduces property for expression")]
-        [DefaultValue(true)]
-        public bool EnableProp { get; set; } = true;
-
-        [Category("Templates")]
+        [Category("Control Flow")]
         [DisplayName("Enable '.switch'")]
         [Description("Produces switch statement")]
         [DefaultValue(true)]
         public bool EnableSwitch { get; set; } = true;
 
-        [Category("Templates")]
-        [DisplayName("Enable '.to'")]
-        [Description("Assigns current expression to some variable")]
-        [DefaultValue(true)]
-        public bool EnableTo { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.tryparse'")]
-        [Description("Parses string as value of some type")]
-        [DefaultValue(true)]
-        public bool EnableTryParse { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.typeof'")]
-        [Description("Wraps type usage with typeof() expression")]
-        [DefaultValue(true)]
-        public bool EnableTypeof { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.using'")]
-        [Description("Wraps resource with using statement")]
-        [DefaultValue(true)]
-        public bool EnableUsing { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.yield'")]
-        [Description("Yields value from iterator method")]
-        [DefaultValue(true)]
-        public bool EnableYield { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.writeline'")]
-        [Description("Writes expression to console output")]
-        [DefaultValue(true)]
-        public bool EnableWriteLine { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.assert'")]
-        [Description("Asserts boolean expression with Debug.Assert")]
-        [DefaultValue(true)]
-        public bool EnableAssert { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.is'")]
-        [Description("Checks expression type with pattern matching")]
-        [DefaultValue(true)]
-        public bool EnableIs { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.as'")]
-        [Description("Casts expression using safe 'as' operator")]
-        [DefaultValue(true)]
-        public bool EnableAs { get; set; } = true;
-
-        [Category("Templates")]
+        [Category("Control Flow")]
         [DisplayName("Enable '.switchexpr'")]
         [Description("Produces switch expression")]
         [DefaultValue(true)]
         public bool EnableSwitchExpr { get; set; } = true;
 
-        [Category("Templates")]
-        [DisplayName("Enable '.discard'")]
-        [Description("Discards expression result")]
-        [DefaultValue(true)]
-        public bool EnableDiscard { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.nameof'")]
-        [Description("Wraps expression with nameof()")]
-        [DefaultValue(true)]
-        public bool EnableNameof { get; set; } = true;
-
-        [Category("Templates")]
-        [DisplayName("Enable '.lambda'")]
-        [Description("Wraps expression in a lambda")]
-        [DefaultValue(true)]
-        public bool EnableLambda { get; set; } = true;
-
-        [Category("Templates")]
+        [Category("Control Flow")]
         [DisplayName("Enable '.conditional'")]
         [Description("Wraps expression with ternary conditional operator")]
         [DefaultValue(true)]
         public bool EnableConditional { get; set; } = true;
 
-        [Category("Templates")]
-        [DisplayName("Enable '.trycatch'")]
-        [Description("Wraps expression with try/catch block")]
-        [DefaultValue(true)]
-        public bool EnableTryCatch { get; set; } = true;
+        // Loops
 
-        [Category("Templates")]
+        [Category("Loops")]
+        [DisplayName("Enable '.for'")]
+        [Description("Iterates over collection with index")]
+        [DefaultValue(true)]
+        public bool EnableFor { get; set; } = true;
+
+        [Category("Loops")]
+        [DisplayName("Enable '.forr'")]
+        [Description("Iterates over collection in reverse with index")]
+        [DefaultValue(true)]
+        public bool EnableForR { get; set; } = true;
+
+        [Category("Loops")]
+        [DisplayName("Enable '.foreach'")]
+        [Description("Iterates over enumerable collection")]
+        [DefaultValue(true)]
+        public bool EnableForEach { get; set; } = true;
+
+        [Category("Loops")]
+        [DisplayName("Enable '.while'")]
+        [Description("Iterates while boolean expression is 'true'")]
+        [DefaultValue(true)]
+        public bool EnableWhile { get; set; } = true;
+
+        // Null and Empty Checks
+
+        [Category("Null and Empty Checks")]
+        [DisplayName("Enable '.null'")]
+        [Description("Checks expression to be null")]
+        [DefaultValue(true)]
+        public bool EnableNull { get; set; } = true;
+
+        [Category("Null and Empty Checks")]
+        [DisplayName("Enable '.notnull'")]
+        [Description("Checks expression to be not null")]
+        [DefaultValue(true)]
+        public bool EnableNotNull { get; set; } = true;
+
+        [Category("Null and Empty Checks")]
         [DisplayName("Enable '.throwifnull'")]
         [Description("Throws if expression is null")]
         [DefaultValue(true)]
         public bool EnableThrowIfNull { get; set; } = true;
 
-        [Category("Templates")]
+        [Category("Null and Empty Checks")]
         [DisplayName("Enable '.notempty'")]
         [Description("Checks if string is null or empty")]
         [DefaultValue(true)]
         public bool EnableNotEmpty { get; set; } = true;
 
-        [Category("Templates")]
+        [Category("Null and Empty Checks")]
         [DisplayName("Enable '.notwhitespace'")]
         [Description("Checks if string is null or whitespace")]
         [DefaultValue(true)]
         public bool EnableNotWhitespace { get; set; } = true;
+
+        // Type Operations
+
+        [Category("Type Operations")]
+        [DisplayName("Enable '.cast'")]
+        [Description("Surrounds expression with cast")]
+        [DefaultValue(true)]
+        public bool EnableCast { get; set; } = true;
+
+        [Category("Type Operations")]
+        [DisplayName("Enable '.is'")]
+        [Description("Checks expression type with pattern matching")]
+        [DefaultValue(true)]
+        public bool EnableIs { get; set; } = true;
+
+        [Category("Type Operations")]
+        [DisplayName("Enable '.as'")]
+        [Description("Casts expression using safe 'as' operator")]
+        [DefaultValue(true)]
+        public bool EnableAs { get; set; } = true;
+
+        [Category("Type Operations")]
+        [DisplayName("Enable '.typeof'")]
+        [Description("Wraps type usage with typeof() expression")]
+        [DefaultValue(true)]
+        public bool EnableTypeof { get; set; } = true;
+
+        [Category("Type Operations")]
+        [DisplayName("Enable '.new'")]
+        [Description("Produces instantiation expression for type")]
+        [DefaultValue(true)]
+        public bool EnableNew { get; set; } = true;
+
+        [Category("Type Operations")]
+        [DisplayName("Enable '.parse'")]
+        [Description("Parses string as value of some type")]
+        [DefaultValue(true)]
+        public bool EnableParse { get; set; } = true;
+
+        [Category("Type Operations")]
+        [DisplayName("Enable '.tryparse'")]
+        [Description("Parses string as value of some type")]
+        [DefaultValue(true)]
+        public bool EnableTryParse { get; set; } = true;
+
+        // Variables and Members
+
+        [Category("Variables and Members")]
+        [DisplayName("Enable '.var'")]
+        [Description("Introduces variable for expression")]
+        [DefaultValue(true)]
+        public bool EnableVar { get; set; } = true;
+
+        [Category("Variables and Members")]
+        [DisplayName("Enable '.field'")]
+        [Description("Introduces field for expression")]
+        [DefaultValue(true)]
+        public bool EnableField { get; set; } = true;
+
+        [Category("Variables and Members")]
+        [DisplayName("Enable '.prop'")]
+        [Description("Introduces property for expression")]
+        [DefaultValue(true)]
+        public bool EnableProp { get; set; } = true;
+
+        [Category("Variables and Members")]
+        [DisplayName("Enable '.to'")]
+        [Description("Assigns current expression to some variable")]
+        [DefaultValue(true)]
+        public bool EnableTo { get; set; } = true;
+
+        [Category("Variables and Members")]
+        [DisplayName("Enable '.inject'")]
+        [Description("Introduces primary constructor parameter of type")]
+        [DefaultValue(true)]
+        public bool EnableInject { get; set; } = true;
+
+        // Expressions
+
+        [Category("Expressions")]
+        [DisplayName("Enable '.not'")]
+        [Description("Negates boolean expression")]
+        [DefaultValue(true)]
+        public bool EnableNot { get; set; } = true;
+
+        [Category("Expressions")]
+        [DisplayName("Enable '.par'")]
+        [Description("Parenthesizes current expression")]
+        [DefaultValue(true)]
+        public bool EnablePar { get; set; } = true;
+
+        [Category("Expressions")]
+        [DisplayName("Enable '.arg'")]
+        [Description("Surrounds expression with invocation")]
+        [DefaultValue(true)]
+        public bool EnableArg { get; set; } = true;
+
+        [Category("Expressions")]
+        [DisplayName("Enable '.await'")]
+        [Description("Awaits expressions of 'Task' type")]
+        [DefaultValue(true)]
+        public bool EnableAwait { get; set; } = true;
+
+        [Category("Expressions")]
+        [DisplayName("Enable '.discard'")]
+        [Description("Discards expression result")]
+        [DefaultValue(true)]
+        public bool EnableDiscard { get; set; } = true;
+
+        [Category("Expressions")]
+        [DisplayName("Enable '.nameof'")]
+        [Description("Wraps expression with nameof()")]
+        [DefaultValue(true)]
+        public bool EnableNameof { get; set; } = true;
+
+        [Category("Expressions")]
+        [DisplayName("Enable '.lambda'")]
+        [Description("Wraps expression in a lambda")]
+        [DefaultValue(true)]
+        public bool EnableLambda { get; set; } = true;
+
+        // Statements
+
+        [Category("Statements")]
+        [DisplayName("Enable '.return'")]
+        [Description("Returns expression from current function")]
+        [DefaultValue(true)]
+        public bool EnableReturn { get; set; } = true;
+
+        [Category("Statements")]
+        [DisplayName("Enable '.throw'")]
+        [Description("Throws expression of 'Exception' type")]
+        [DefaultValue(true)]
+        public bool EnableThrow { get; set; } = true;
+
+        [Category("Statements")]
+        [DisplayName("Enable '.trycatch'")]
+        [Description("Wraps expression with try/catch block")]
+        [DefaultValue(true)]
+        public bool EnableTryCatch { get; set; } = true;
+
+        [Category("Statements")]
+        [DisplayName("Enable '.using'")]
+        [Description("Wraps resource with using statement")]
+        [DefaultValue(true)]
+        public bool EnableUsing { get; set; } = true;
+
+        [Category("Statements")]
+        [DisplayName("Enable '.lock'")]
+        [Description("Surrounds expression with lock block")]
+        [DefaultValue(true)]
+        public bool EnableLock { get; set; } = true;
+
+        [Category("Statements")]
+        [DisplayName("Enable '.yield'")]
+        [Description("Yields value from iterator method")]
+        [DefaultValue(true)]
+        public bool EnableYield { get; set; } = true;
+
+        // Diagnostics
+
+        [Category("Diagnostics")]
+        [DisplayName("Enable '.writeline'")]
+        [Description("Writes expression to console output")]
+        [DefaultValue(true)]
+        public bool EnableWriteLine { get; set; } = true;
+
+        [Category("Diagnostics")]
+        [DisplayName("Enable '.assert'")]
+        [Description("Asserts boolean expression with Debug.Assert")]
+        [DefaultValue(true)]
+        public bool EnableAssert { get; set; } = true;
 
         public bool IsTemplateEnabled(string templateName)
         {
